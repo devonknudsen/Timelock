a56610d106bc23514721e36d53ed430fdd442cad
ZackaryPhillips <37816812+ZackaryPhillips@users.noreply.github.com>
2019-10-22 10:51:16 -0500

    Update Timelock.py

--- Timelock.py
+++ Timelock.py
@@ -1,24 +1,17 @@
+ 
 import time
 import hashlib
-import sys
 
-DEBUG = True
+DEBUG = False
 
 INTERVAL = 60
+ctime = "2017 03 23 18 02 06"
 
-epoch = input()
-epoch = time.strptime(epoch, "%Y %m %d %H %M %S")
-START_TIME = time.mktime(epoch)
-while(True):
-    TIME = time.time()
-    #TIME = time.mktime(time.strptime( "2017 03 23 18 02 06","%Y %m %d %H %M %S"))
-    CURRENT_TIME = int(START_TIME - TIME)
-    print("current system time: {}\n".format(time.ctime()))
-    first_hash = hashlib.md5(str(CURRENT_TIME).encode()).hexdigest()
-    if DEBUG:
-        print(first_hash)
+def double_md5(elapsed):
+    first_hash = hashlib.md5(str(elapsed).encode()).hexdigest()
     second_hash = hashlib.md5(str(first_hash).encode()).hexdigest()
     if DEBUG:
+        print(first_hash)
         print(second_hash)
     letters = ''
     numbers = ''
@@ -34,4 +27,30 @@ while(True):
             count_numbers += 1
     password = letters + numbers
     print(password)
-    time.sleep(INTERVAL)
+    print()
+
+
+epoch = input()
+epoch = time.strptime(epoch, "%Y %m %d %H %M %S")
+START_TIME = time.mktime(epoch)
+if DEBUG:
+    print("Start time: {}".format(START_TIME))
+# set_current_time = time.time()
+set_current_time = time.strptime( ctime ,"%Y %m %d %H %M %S")
+CURRENT_TIME = time.mktime(set_current_time)
+if DEBUG:
+    print("Current time: {}".format(CURRENT_TIME))
+TIME_ELAPSED = int(CURRENT_TIME - START_TIME )
+print("current system time: {} {} {} {} {} {}\n".format(set_current_time.tm_year, set_current_time.tm_mon, set_current_time.tm_mday, set_current_time.tm_hour, set_current_time.tm_min, set_current_time.tm_sec))
+double_md5(TIME_ELAPSED - (TIME_ELAPSED % INTERVAL))
+while(True):
+    if DEBUG:
+        print("Time elapsed: {}".format(TIME_ELAPSED))
+    if((TIME_ELAPSED % INTERVAL) ==  0):
+        print("Current sytem time: {}\n".format(time.ctime(CURRENT_TIME)))
+        double_md5(TIME_ELAPSED)
+    time.sleep(1)
+    TIME_ELAPSED += 1
+    CURRENT_TIME += 1
+
+    
